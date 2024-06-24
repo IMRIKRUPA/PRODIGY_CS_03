7fe8a4c9a4cb4ba931f819a600e4b6b00201c536
IMRIKRUPA <148330196+IMRIKRUPA@users.noreply.github.com>
2024-06-24 10:36:28 +0530

    Add files via upload

---
+++ CS-3output.py
@@ -0,0 +1,12 @@
+/Users/imrikrupa/PycharmProjects/pythonProject/venv/bin/python /Users/imrikrupa/PycharmProjects/pythonProject/CS-3.py 
+Enter a password to check its complexity: 12345
+Password: 12345
+Length: 5
+Has Uppercase: False
+Has Lowercase: False
+Has Digit: True
+Has Special Character: False
+Strength: Weak
+Feedback: Your password is weak. Password should be at least 8 characters long. Password should include at least one uppercase letter. Password should include at least one lowercase letter. Password should include at least one special character (e.g., !, @, #, $, etc.).
+
+Process finished with exit code 0
